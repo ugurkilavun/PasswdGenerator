bda3e325da5e868af83cb16cfb039945a36f9d42
ugurkilavun <ugurkilavun+github@hotmail.com>
2025-02-17 09:40:04 +0300

    Update layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,56 +9,56 @@ const passwdSafetyInformation = document.getElementById("passwdSafetyInformation
 
 // ?
 window.onload = () => {
-	setSecurityStatus(inputPasswordRange.value);
-    localStorage.setItem("lowerCase", true);
-    localStorage.getItem("upperCase") == "true" ? document.getElementById("upperCaseCB").checked = true : document.getElementById("upperCaseCB").checked = false;
-    // localStorage.getItem("lowerCase") == "true" ? document.getElementById("lowerCaseCB").checked = true : document.getElementById("lowerCaseCB").checked = false;
-    localStorage.getItem("number") == "true" ? document.getElementById("numbersCB").checked = true : document.getElementById("numbersCB").checked = false;
-    localStorage.getItem("randomCharacter") == "true" ? document.getElementById("randomCharactersCB").checked = true : document.getElementById("randomCharactersCB").checked = false;
-    generatePasswd();
-    setTheme(localStorage.getItem("theme") == "dark" ? true : false);
+  setSecurityStatus(inputPasswordRange.value);
+  localStorage.setItem("lowerCase", true);
+  localStorage.getItem("upperCase") == "true" ? document.getElementById("upperCaseCB").checked = true : document.getElementById("upperCaseCB").checked = false;
+  // localStorage.getItem("lowerCase") == "true" ? document.getElementById("lowerCaseCB").checked = true : document.getElementById("lowerCaseCB").checked = false;
+  localStorage.getItem("number") == "true" ? document.getElementById("numbersCB").checked = true : document.getElementById("numbersCB").checked = false;
+  localStorage.getItem("randomCharacter") == "true" ? document.getElementById("randomCharactersCB").checked = true : document.getElementById("randomCharactersCB").checked = false;
+  generatePasswd();
+  setTheme(localStorage.getItem("theme") == "dark" ? true : false);
 }
 
 passwordRangeValue.textContent = inputPasswordRange.value;
 inputPasswordRange.addEventListener("change", (event) => {
-    passwordRangeValue.textContent = event.target.value;
+  passwordRangeValue.textContent = event.target.value;
 });
 
 increaseThePasswordRange.addEventListener("click", () => {
-    inputPasswordRange.value++;
-    passwordRangeValue.textContent = inputPasswordRange.value;
-    setSecurityStatus(inputPasswordRange.value);
+  inputPasswordRange.value++;
+  passwordRangeValue.textContent = inputPasswordRange.value;
+  setSecurityStatus(inputPasswordRange.value);
 });
 
 decreaseThePasswordRange.addEventListener("click", () => {
-    inputPasswordRange.value--;
-    passwordRangeValue.textContent = inputPasswordRange.value;
-    setSecurityStatus(inputPasswordRange.value);
+  inputPasswordRange.value--;
+  passwordRangeValue.textContent = inputPasswordRange.value;
+  setSecurityStatus(inputPasswordRange.value);
 });
 
 // Password Range Settings: Security Status
 inputPasswordRange.addEventListener('change', () => {
-    setSecurityStatus(inputPasswordRange.value);
+  setSecurityStatus(inputPasswordRange.value);
 });
 
 // Function: Security Status
 const setSecurityStatus = (securityValue) => {
-    if (securityValue <= 6) {
-        passwdSafetyInformation.style.backgroundColor = "#ffdc9d";
-        passwdSafetyInformation.innerHTML = "Weak";
-    }
-    else if (securityValue >= 7 && securityValue <= 9) {
-        passwdSafetyInformation.style.background = "#fafa8a"; /* fbfb9e,fcfcb5 */
-        passwdSafetyInformation.innerHTML = "Good";
-    }
-    else if (securityValue >= 10 && securityValue <= 13) {
-        passwdSafetyInformation.style.background = "#c5f9c5";
-        passwdSafetyInformation.innerHTML = "Strong";
-    }
-    else if (securityValue >= 14) {
-        passwdSafetyInformation.style.background = "#90EE90";
-        passwdSafetyInformation.innerHTML = "Very Strong";
-    }
+  if (securityValue <= 6) {
+    passwdSafetyInformation.style.backgroundColor = "#ffdc9d";
+    passwdSafetyInformation.innerHTML = "Weak";
+  }
+  else if (securityValue >= 7 && securityValue <= 9) {
+    passwdSafetyInformation.style.background = "#fafa8a"; /* fbfb9e,fcfcb5 */
+    passwdSafetyInformation.innerHTML = "Good";
+  }
+  else if (securityValue >= 10 && securityValue <= 13) {
+    passwdSafetyInformation.style.background = "#c5f9c5";
+    passwdSafetyInformation.innerHTML = "Strong";
+  }
+  else if (securityValue >= 14) {
+    passwdSafetyInformation.style.background = "#90EE90";
+    passwdSafetyInformation.innerHTML = "Very Strong";
+  }
 }
 
 // -_?
@@ -66,25 +66,24 @@ const setSecurityStatus = (securityValue) => {
 // *More Settings Modal 
 let moreSettingsModalToggleState = false; // Default: false == "display: none;" 
 const setMoreSettingsModal = () => {
-    moreSettingsModalToggleState = !moreSettingsModalToggleState;
-    const moreSettingsModal = document.getElementById("setMoreSettingsModal");
-    const moreSettingsButtonWrapper= document.getElementById("moreSettingsButtonWrapper");
+  moreSettingsModalToggleState = !moreSettingsModalToggleState;
+  const moreSettingsModal = document.getElementById("setMoreSettingsModal");
+  const moreSettingsButtonWrapper = document.getElementById("moreSettingsButtonWrapper");
 
-    if (moreSettingsModalToggleState) {
-        moreSettingsModal.style.display = 'flex';
-        // moreSettingsButtonWrapper.style.marginLeft = "440px";
-        setTimeout(() => {
-            moreSettingsModal.classList.add('show');
-            
-        }, 100);
-    }
-    else {
-        moreSettingsModal.classList.remove('show');
-        setTimeout(() => {
-            // moreSettingsButtonWrapper.style.marginLeft = "440px";
-            moreSettingsModal.style.display = 'none';
-        }, 500);
-    }
+  if (moreSettingsModalToggleState) {
+    moreSettingsModal.style.display = 'flex';
+    // moreSettingsButtonWrapper.style.marginLeft = "440px";
+    setTimeout(() => {
+      moreSettingsModal.classList.add('show');
+    }, 100);
+  }
+  else {
+    moreSettingsModal.classList.remove('show');
+    setTimeout(() => {
+      // moreSettingsButtonWrapper.style.marginLeft = "440px";
+      moreSettingsModal.style.display = 'none';
+    }, 500);
+  }
 }
 
 // *Generate Password
@@ -94,14 +93,14 @@ const copyPasswd = document.getElementById("copyPasswd");
 const copyNotification = document.getElementById("copyNotification");
 
 generatePasswdButton.addEventListener('click', () => {
-    generatePasswd(); 
+  generatePasswd();
 });
 
 // 0_0
 
 // Upper Case
 document.getElementById("upperCaseCB").addEventListener('change', () => {
-    document.getElementById("upperCaseCB").checked ? localStorage.setItem("upperCase", true) : localStorage.setItem("upperCase", false);
+  document.getElementById("upperCaseCB").checked ? localStorage.setItem("upperCase", true) : localStorage.setItem("upperCase", false);
 });
 /* Lower Case
 document.getElementById("lowerCaseCB").addEventListener('change', () => {
@@ -109,96 +108,96 @@ document.getElementById("lowerCaseCB").addEventListener('change', () => {
 }); */
 // Number
 document.getElementById("numbersCB").addEventListener('change', () => {
-    document.getElementById("numbersCB").checked ? localStorage.setItem("number", true) : localStorage.setItem("number", false);
+  document.getElementById("numbersCB").checked ? localStorage.setItem("number", true) : localStorage.setItem("number", false);
 });
 // Random Character
 document.getElementById("randomCharactersCB").addEventListener('change', () => {
-    document.getElementById("randomCharactersCB").checked ? localStorage.setItem("randomCharacter", true) : localStorage.setItem("randomCharacter", false);
+  document.getElementById("randomCharactersCB").checked ? localStorage.setItem("randomCharacter", true) : localStorage.setItem("randomCharacter", false);
 });
 
 
 // Function: Generate Password
 const generatePasswd = () => {
-    let upperCaseCB, lowerCaseCB, numbersCB, randomCharactersCB = false;
-    let randomPasswd = [];
-    let passLength = inputPasswordRange.value;
-    let myAsciiCodes = {
-        100: "A", 101: "B", 102: "C", 103: "D", 104: "E",
-        105: "F", 106: "G", 107: "H", 108: "I", 109: "J",
-        110: "K", 111: "L", 112: "M", 113: "N", 114: "O",
-        115: "P", 116: "Q", 117: "R", 118: "S", 119: "T",
-        120: "U", 121: "V", 122: "W", 123: "X", 124: "Y",
-        125: "Z", 126: "a", 127: "b", 128: "c", 129: "d",
-        130: "e", 131: "f", 132: "g", 133: "h", 134: "i",
-        135: "j", 136: "k", 137: "l", 138: "m", 139: "n",
-        140: "o", 141: "p", 142: "q", 143: "r", 144: "s",
-        145: "t", 146: "u", 147: "v", 148: "w", 149: "x",
-        150: "y", 151: "z", 152: "0", 153: "1", 154: "2",
-        155: "3", 156: "4", 157: "5", 158: "6", 159: "7",
-        160: "8", 161: "9", 162: "@", 163: "#", 164: "$",
-        165: "%", 166: "^", 167: "&", 168: "*", 169: "(",
-        170: ")", 171: "-", 172: "_", 173: "=", 174: "+",
-        175: "[", 176: "]", 177: "{", 178: "}", 179: "|",
-        180: ";", 181: ":", 182: "'", 183: "\"", 184: "<",
-        185: ">", 186: ",", 187: ".", 188: "?", 189: "/",
-        190: "`", 191: "~", 192: "!", 193: "€"
-    };
-    /*
-        ABC: 100 - 125
-        abc: 126 - 151
-        123: 152 - 161
-        %#^%: 162 - 193
-    */
+  let upperCaseCB, lowerCaseCB, numbersCB, randomCharactersCB = false;
+  let randomPasswd = [];
+  let passLength = inputPasswordRange.value;
+  let myAsciiCodes = {
+    100: "A", 101: "B", 102: "C", 103: "D", 104: "E",
+    105: "F", 106: "G", 107: "H", 108: "I", 109: "J",
+    110: "K", 111: "L", 112: "M", 113: "N", 114: "O",
+    115: "P", 116: "Q", 117: "R", 118: "S", 119: "T",
+    120: "U", 121: "V", 122: "W", 123: "X", 124: "Y",
+    125: "Z", 126: "a", 127: "b", 128: "c", 129: "d",
+    130: "e", 131: "f", 132: "g", 133: "h", 134: "i",
+    135: "j", 136: "k", 137: "l", 138: "m", 139: "n",
+    140: "o", 141: "p", 142: "q", 143: "r", 144: "s",
+    145: "t", 146: "u", 147: "v", 148: "w", 149: "x",
+    150: "y", 151: "z", 152: "0", 153: "1", 154: "2",
+    155: "3", 156: "4", 157: "5", 158: "6", 159: "7",
+    160: "8", 161: "9", 162: "@", 163: "#", 164: "$",
+    165: "%", 166: "^", 167: "&", 168: "*", 169: "(",
+    170: ")", 171: "-", 172: "_", 173: "=", 174: "+",
+    175: "[", 176: "]", 177: "{", 178: "}", 179: "|",
+    180: ";", 181: ":", 182: "'", 183: "\"", 184: "<",
+    185: ">", 186: ",", 187: ".", 188: "?", 189: "/",
+    190: "`", 191: "~", 192: "!", 193: "€"
+  };
+  /*
+      ABC: 100 - 125
+      abc: 126 - 151
+      123: 152 - 161
+      %#^%: 162 - 193
+  */
 
-    document.querySelector('#upperCaseCB:checked') ? upperCaseCB = true : upperCaseCB = false;
-    document.querySelector('#lowerCaseCB:checked') ? lowerCaseCB = true : lowerCaseCB = false;
-    document.querySelector('#numbersCB:checked') ? numbersCB = true : numbersCB = false;
-    document.querySelector('#randomCharactersCB:checked') ? randomCharactersCB = true : randomCharactersCB = false;
+  document.querySelector('#upperCaseCB:checked') ? upperCaseCB = true : upperCaseCB = false;
+  document.querySelector('#lowerCaseCB:checked') ? lowerCaseCB = true : lowerCaseCB = false;
+  document.querySelector('#numbersCB:checked') ? numbersCB = true : numbersCB = false;
+  document.querySelector('#randomCharactersCB:checked') ? randomCharactersCB = true : randomCharactersCB = false;
 
-    while (passLength > 0){
-        let randomPasswdCode = Math.trunc(Math.random() * 1000);
-        if (randomPasswdCode >= 100 && randomPasswdCode <= 193){
-            // ABC
-            if (randomPasswdCode >= 100 && randomPasswdCode <= 125 && upperCaseCB) {
-                randomPasswd.push(myAsciiCodes[randomPasswdCode]);
-                passLength--;
-            }
-            // abc
-            else if (randomPasswdCode >= 126 && randomPasswdCode <= 151 && lowerCaseCB) {
-                randomPasswd.push(myAsciiCodes[randomPasswdCode]);
-                passLength--;
-            }
-            // 123
-            else if (randomPasswdCode >= 152 && randomPasswdCode <= 161 && numbersCB) {
-                randomPasswd.push(myAsciiCodes[randomPasswdCode]);
-                passLength--;
-            }
-            // %#^%
-            else if (randomPasswdCode >= 162 && randomPasswdCode <= 193 && randomCharactersCB) {
-                randomPasswd.push(myAsciiCodes[randomPasswdCode]);
-                passLength--;
-            }
-        }  
+  while (passLength > 0) {
+    let randomPasswdCode = Math.trunc(Math.random() * 1000);
+    if (randomPasswdCode >= 100 && randomPasswdCode <= 193) {
+      // ABC
+      if (randomPasswdCode >= 100 && randomPasswdCode <= 125 && upperCaseCB) {
+        randomPasswd.push(myAsciiCodes[randomPasswdCode]);
+        passLength--;
+      }
+      // abc
+      else if (randomPasswdCode >= 126 && randomPasswdCode <= 151 && lowerCaseCB) {
+        randomPasswd.push(myAsciiCodes[randomPasswdCode]);
+        passLength--;
+      }
+      // 123
+      else if (randomPasswdCode >= 152 && randomPasswdCode <= 161 && numbersCB) {
+        randomPasswd.push(myAsciiCodes[randomPasswdCode]);
+        passLength--;
+      }
+      // %#^%
+      else if (randomPasswdCode >= 162 && randomPasswdCode <= 193 && randomCharactersCB) {
+        randomPasswd.push(myAsciiCodes[randomPasswdCode]);
+        passLength--;
+      }
     }
-   
-    passwdGenTextInput.value = randomPasswd.join('');  
+  }
+
+  passwdGenTextInput.value = randomPasswd.join('');
 }
 
 
 // Copy the passwd
 copyPasswd.addEventListener('click', () => {
-    // !Delete: passwdGenTextInput.select();
-    // !Delete: passwdGenTextInput.setSelectionRange(0, 99999); // For mobile device
-    // Copy the text
-    navigator.clipboard.writeText(passwdGenTextInput.value).then(() => {
-        copyNotification.style.visibility = "visible";
-        setTimeout(() => {
-            copyNotification.style.visibility = "hidden";
-        }, 1200); // 1.2 Sec.
-    }).catch(err => {
-       alert('An error occurred during copying: ', err);
-    });
-    
+  // !Delete: passwdGenTextInput.select();
+  // !Delete: passwdGenTextInput.setSelectionRange(0, 99999); // For mobile device
+  // Copy the text
+  navigator.clipboard.writeText(passwdGenTextInput.value).then(() => {
+    copyNotification.style.visibility = "visible";
+    setTimeout(() => {
+      copyNotification.style.visibility = "hidden";
+    }, 1200); // 1.2 Sec.
+  }).catch(err => {
+    alert('An error occurred during copying: ', err);
+  });
+
 });
 
 
@@ -207,21 +206,21 @@ copyPasswd.addEventListener('click', () => {
 
 // Theme
 document.getElementById("themeSettings").addEventListener('change', () => {
-    setTheme(document.querySelector("#themeSettings:checked") ? true : false);
+  setTheme(document.querySelector("#themeSettings:checked") ? true : false);
 });
 
 // Function; Set Theme
 const setTheme = (trueOrFalse) => {
-    if(trueOrFalse) {
-        document.body.classList.toggle("dark-theme");
-        localStorage.setItem("theme", "dark");
-        document.getElementById("themeSettings").checked = true;
-    }
-    else {
-        document.body.classList.remove("dark-theme");
-        localStorage.setItem("theme", "light");
-        document.getElementById("themeSettings").checked = false;
-    }
+  if (trueOrFalse) {
+    document.body.classList.toggle("dark-theme");
+    localStorage.setItem("theme", "dark");
+    document.getElementById("themeSettings").checked = true;
+  }
+  else {
+    document.body.classList.remove("dark-theme");
+    localStorage.setItem("theme", "light");
+    document.getElementById("themeSettings").checked = false;
+  }
 }
 
 /*
